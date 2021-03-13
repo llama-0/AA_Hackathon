ff494b66a28b2642381bd4786030aec6181e5a29
xrseventy <5526958@gmail.com>
2021-03-13 14:54:18 +0300

    rename presentations

--- app/src/main/java/com/hackatron52/androidacademyhackathon/presentation/fragments/PlaceDetailsFragment.kt
+++ app/src/main/java/com/hackatron52/androidacademyhackathon/presentation/fragments/PlaceDetailsFragment.kt
@@ -1,4 +1,4 @@
-package com.hackatron52.androidacademyhackathon
+package com.hackatron52.androidacademyhackathon.presentation.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,6 +8,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.hackatron52.androidacademyhackathon.R
 
 class PlaceDetailsFragment : Fragment() {
 
